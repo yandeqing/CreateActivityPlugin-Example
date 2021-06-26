9d873102c9c52b3f226bae4957b3e8fac037bf53
yandeqing <546218945@qq.com>
2021-06-26 16:32:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tsnt/app/LauncherActivity.java
@@ -0,0 +1,16 @@
+package com.example.tsnt.app;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tsnt.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
